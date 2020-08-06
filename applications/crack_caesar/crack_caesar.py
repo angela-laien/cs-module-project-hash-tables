c8384902ad95be307317cb6b22982c85b44b8c8a
Laien Xie <angela@Laiens-MacBook-Air.local>
2020-08-06 16:03:33 -0700

    day 4

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -2,4 +2,32 @@
 # decode it.
 
 # Your code here
+ciphertext = open('applications/crack_caesar/ciphertext.txt').read()
+letters = 'ETAOHNRISDLWUGFBMYCPKVQJXZ'
 
+def calculate_freq(txt):
+    txt = txt.upper()
+    freq = {}
+
+    for l in letters:
+        freq[l] = 0
+    for l in txt:
+        if l in letters:
+            freq[l] += 1
+    return freq
+
+frequency = calculate_freq(ciphertext)
+freq_list = ''.join(sorted(frequency, key=lambda f: frequency[f], reverse=True))
+print(freq_list)
+
+def decode(txt):
+    plaintext = ''
+    for t in txt:
+        i = freq_list.find(t)
+        if letters[i] == 'Z':
+            plaintext += ' '
+        else:
+            plaintext += letters[i]
+    return plaintext
+
+print(decode(ciphertext))
